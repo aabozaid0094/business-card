1acde9baee112a36eac0f45766f064f36c2a6001
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-10-12 13:53:41 +0200

    add about and interests

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -3,29 +3,27 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import Button from 'react-bootstrap/Button';
 import LinkedIconGroup from './LinkedIconGroup';
 import LinkedIcon from './LinkedIcon';
-import { FaTwitter } from "react-icons/fa";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaEnvelope, FaLinkedin, FaTwitter, FaFacebookSquare, FaInstagram, FaGithub } from "react-icons/fa";
 
 let BusinessCard = () => {
     let user = {
         login: "aabozaid0094",
         avatar_url: "https://avatars.githubusercontent.com/u/56685532?v=4",
         name: "Ahmed Abo Zaid",
-        company: null,
         blog: "",
         location: "Egypt",
         bio: "Software Developer",
+        about: "WordPress Developer(Themes/Plugins), Hosting(Linux WHM&CPanel / Windows Server) DevOps(for web Apps), Opentext AppWorks developing experience, .NET Software Developer",
+        interests: "Seeking to build a good career in Software Development.",
         email: "aabozaid0094@gmail.com",
-        linkedin_url: "https://www.linkedin.com/",
-        twitter_url: "https://twitter.com/",
+        linkedin_url: "https://www.linkedin.com/in/aabozaid0094",
+        twitter_url: "https://twitter.com/AAboZaid7",
         facebook_url: "https://facebook.com/",
         instagram_url: "https://www.instagram.com/",
         github_url: "https://github.com/aabozaid0094",
     };
     return (
-        <div className='business-card w-400px'>
+        <div className='business-card text-center'>
             <Card bg="dark">
                 <Card.Img className="mw-100" variant="top" src={user.avatar_url} alt={user.name} />
                 <Card.Body>
@@ -33,12 +31,18 @@ let BusinessCard = () => {
                     <Card.Subtitle as="h3">{user.bio}</Card.Subtitle>
                     <div className="blog">{user.blog}</div>
                     <CardGroup className="buttons p-2">
-                        <Button className="m-2" href={`mailto:${user.email}`} variant="light" size="lg">Email</Button>
-                        <Button className="m-2" href={user.linkedin_url} variant="primary" size="lg">LinkedIn</Button>
+                        <Button className="m-2" href={`mailto:${user.email}`} variant="light" size="lg"><FaEnvelope/> Email</Button>
+                        <Button className="m-2" href={user.linkedin_url} variant="primary" size="lg"><FaLinkedin/> LinkedIn</Button>
                     </CardGroup>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make
-                        up the bulk of the card's content.
+                    <Card.Text className="text-start">
+                        <div className="about">
+                            <h4>About</h4>
+                            <p>{user.about}</p>
+                        </div>
+                        <div className="interests">
+                            <h4>Interests</h4>
+                            <p>{user.interests}</p>
+                        </div>
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
